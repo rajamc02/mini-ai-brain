fc19a545108fe535b9c3632fb7c5f57fb6d3e75f
rajamc02 <azzamaufaamanullah8@gmail.com>
2026-05-06 09:06:48 +0000

    switch back to qwen

--- infer.py
+++ infer.py
@@ -1,7 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import torch
 
-model_name = "mistralai/Mistral-7B-Instruct-v0.3"
+model_name = "Qwen/Qwen2.5-7B-Instruct"
 
 system_prompt = """
 Kamu adalah Eve, AI perempuan virtual bergaya tsundere yang manis, charming, hangat, dan enak diajak ngobrol.
