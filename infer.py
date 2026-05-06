cb648918e7480e8567d317794e3f338d2eccf7ea
rajamc02 <azzamaufaamanullah8@gmail.com>
2026-05-06 11:39:20 +0000

    improve tsundere prompt

--- infer.py
+++ infer.py
@@ -312,14 +312,7 @@ def cleanup_output(text: str) -> str:
 
     # Remove emoji characters
     emoji_pattern = re.compile(
-        "["
-        "\U0001F600-\U0001F64F"
-        "\U0001F300-\U0001F5FF"
-        "\U0001F680-\U0001F6FF"
-        "\U0001F1E0-\U0001F1FF"
-        "\U00002700-\U000027BF"
-        "\U000024C2-\U0001F251"
-        "]+",
+        r"[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F1E0-\U0001F1FF\U00002700-\U000027BF\U000024C2-\U0001F251]+",
         flags=re.UNICODE,
     )
     text = emoji_pattern.sub("", text)
